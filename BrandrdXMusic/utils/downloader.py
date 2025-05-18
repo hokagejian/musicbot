3f0ef78e611ce0a7358e2f3767e7d869ed686fd4
idk20256 <emailmati98@gmail.com>
2025-05-18 07:18:40 +0700

    Update downloader.py

--- BrandrdXMusic/utils/downloader.py
+++ BrandrdXMusic/utils/downloader.py
@@ -2,15 +2,17 @@ from os import path
 import yt_dlp
 from yt_dlp.utils import DownloadError
 
-ytdl = yt_dlp.YoutubeDL(
-    {
-        "outtmpl": "downloads/%(id)s.%(ext)s",
-        "format": "bestaudio[ext=m4a]",
-        "geo_bypass": True,
-        "nocheckcertificate": True,
-    }
-)
+COOKIES_PATH = "BrandedXmusic.txt"  # <-- Path ke file cookies hasil ekspor browser
 
+ytdl_opts = {
+    "outtmpl": "downloads/%(id)s.%(ext)s",
+    "format": "bestaudio[ext=m4a]",
+    "geo_bypass": True,
+    "nocheckcertificate": True,
+    "cookies": COOKIES_PATH,  # <-- Tambahkan ini
+}
+
+ytdl = yt_dlp.YoutubeDL(ytdl_opts)
 
 def download(url: str, my_hook) -> str:
     ydl_optssx = {
@@ -20,6 +22,7 @@ def download(url: str, my_hook) -> str:
         "nocheckcertificate": True,
         "quiet": True,
         "no_warnings": True,
+        "cookies": COOKIES_PATH,  # <-- Tambahkan ini juga di sini
     }
     info = ytdl.extract_info(url, False)
     try:
@@ -27,7 +30,8 @@ def download(url: str, my_hook) -> str:
         x.add_progress_hook(my_hook)
         dloader = x.download([url])
     except Exception as y_e:
-        return print(y_e)
+        print(y_e)
+        return None
     else:
         dloader
     xyz = path.join("downloads", f"{info['id']}.{info['ext']}")
