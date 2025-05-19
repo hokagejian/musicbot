0063ff3116fa91b4f3367846540e0920ba1845a4
idk20256 <emailmati98@gmail.com>
2025-05-19 20:08:26 +0700

    Update call.py

--- BrandrdXMusic/core/call.py
+++ BrandrdXMusic/core/call.py
@@ -1,3 +1,92 @@
+#test pytgcall terbaruu
+```
+import asyncio
+import os
+from datetime import datetime, timedelta
+from typing import Union
+from pyrogram import Client
+from pyrogram.types import InlineKeyboardMarkup
+from ntgcalls import TelegramServerError
+from pytgcalls import PyTgCalls
+from pytgcalls.exceptions import (
+    GroupCallNotExistError,
+    NoActiveGroupCall,
+)
+from pytgcalls.types import (
+    MediaStream,
+    AudioQuality,
+    VideoQuality,
+    Update,
+)
+from pytgcalls.types.stream import StreamAudioEnded
+import config
+from BrandrdXMusic import LOGGER, YouTube, app
+from BrandrdXMusic.misc import db
+from BrandrdXMusic.utils.database import (
+    add_active_chat,
+    add_active_video_chat,
+    get_lang,
+    get_loop,
+    group_assistant,
+    is_autoend,
+    music_on,
+    remove_active_chat,
+    remove_active_video_chat,
+    set_loop,
+)
+from BrandrdXMusic.utils.exceptions import AssistantErr
+from BrandrdXMusic.utils.formatters import check_duration, seconds_to_min, speed_converter
+from BrandrdXMusic.utils.inline.play import stream_markup, stream_markup2
+from BrandrdXMusic.utils.stream.autoclear import auto_clean
+from BrandrdXMusic.utils.thumbnails import get_thumb
+from strings import get_string
+
+autoend = {}
+counter = {}
+loop = asyncio.get_event_loop_policy().get_event_loop()
+
+async def _clear_(chat_id):
+    db[chat_id] = []
+    await remove_active_video_chat(chat_id)
+    await remove_active_chat(chat_id)
+
+class Call(PyTgCalls):
+    def __init__(self):
+        self.userbot1 = Client(
+            name="BrandrdXMusic1",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING1),
+        )
+        self.one = PyTgCalls(
+            self.userbot1,
+            cache_duration=100,
+        )
+        self.userbot2 = Client(
+            name="BrandrdXMusic2",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING2),
+        )
+        self.two = PyTgCalls(
+            self.userbot2,
+            cache_duration=100,
+        )
+        self.userbot3 = Client(
+            name="BrandrdXMusic3",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING3),
+        )
+        self.three = PyTgCalls(
+            self.userbot3,
+            cache_duration=100,
+        )
+        self.userbot4
+```
+[19/5, 20.05] Meta AI: Berikut adalah kode lengkap `call.py` yang telah diperbaiki:
+
+```
 import asyncio
 import os
 from datetime import datetime, timedelta
@@ -153,5 +242,5 @@ class Call(PyTgCalls):
             pass
         try:
             if config.STRING5:
-                await self.five.leave_group_call(chat
+                await self.five
 ```
