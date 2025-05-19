30c658e585bf59a72a5bff5aa6f87c11a77649dd
idk20256 <emailmati98@gmail.com>
2025-05-19 20:32:14 +0700

    Update call.py

--- BrandrdXMusic/core/call.py
+++ BrandrdXMusic/core/call.py
@@ -1,146 +1,134 @@
-import asyncio
-import os
-from datetime import datetime, timedelta
-from typing import Union
-from pyrogram import Client
-from pyrogram.types import InlineKeyboardMarkup
-from ntgcalls import TelegramServerError
-from pytgcalls import PyTgCalls
-from pytgcalls.exceptions import (
-    GroupCallNotExistError,
-    NoActiveGroupCall,
-)
-from pytgcalls.types import (
-    MediaStream,
-    AudioQuality,
-    VideoQuality,
-    Update,
-)
-from pytgcalls.types.stream import StreamAudioEnded
-import config
-from BrandrdXMusic import LOGGER, YouTube, app
-from BrandrdXMusic.misc import db
-from BrandrdXMusic.utils.database import (
-    add_active_chat,
-    add_active_video_chat,
-    get_lang,
-    get_loop,
-    group_assistant,
-    is_autoend,
-    music_on,
-    remove_active_chat,
-    remove_active_video_chat,
-    set_loop,
-)
-from BrandrdXMusic.utils.exceptions import AssistantErr
-from BrandrdXMusic.utils.formatters import check_duration, seconds_to_min, speed_converter
-from BrandrdXMusic.utils.inline.play import stream_markup, stream_markup2
-from BrandrdXMusic.utils.stream.autoclear import auto_clean
-from BrandrdXMusic.utils.thumbnails import get_thumb
-from strings import get_string
+import asyncio 
+import os 
+from datetime import datetime, timedelta 
+from typing import Union 
+from pyrogram import Client 
+from pyrogram.types import InlineKeyboardMarkup 
+from ntgcalls import TelegramServerError 
+from pytgcalls import PyTgCalls 
+from pytgcalls.exceptions import NoActiveGroupCall
+from pytgcalls.types import ( 
+    MediaStream, 
+    AudioQuality, 
+    VideoQuality, 
+    Update, 
+) 
+from pytgcalls.types.stream import StreamAudioEnded 
+import config 
+from BrandrdXMusic import LOGGER, YouTube, app 
+from BrandrdXMusic.misc import db 
+from BrandrdXMusic.utils.database import ( 
+    add_active_chat, 
+    add_active_video_chat, 
+    get_lang, 
+    get_loop, 
+    group_assistant, 
+    is_autoend, 
+    music_on, 
+    remove_active_chat, 
+    remove_active_video_chat, 
+    set_loop, 
+) 
+from BrandrdXMusic.utils.exceptions import AssistantErr 
+from BrandrdXMusic.utils.formatters import check_duration, seconds_to_min, speed_converter 
+from BrandrdXMusic.utils.inline.play import stream_markup, stream_markup2 
+from BrandrdXMusic.utils.stream.autoclear import auto_clean 
+from BrandrdXMusic.utils.thumbnails import get_thumb 
+from strings import get_string 
 
-autoend = {}
-counter = {}
-loop = asyncio.get_event_loop_policy().get_event_loop()
+autoend = {} 
+counter = {} 
+loop = asyncio.get_event_loop_policy().get_event_loop() 
 
-async def _clear_(chat_id):
-    db[chat_id] = []
-    await remove_active_video_chat(chat_id)
-    await remove_active_chat(chat_id)
+async def _clear_(chat_id): 
+    db[chat_id] = [] 
+    await remove_active_video_chat(chat_id) 
+    await remove_active_chat(chat_id) 
 
-class Call(PyTgCalls):
-    def __init__(self):
-        self.userbot1 = Client(
-            name="BrandrdXMusic1",
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
-            session_string=str(config.STRING1),
-        )
-        self.one = PyTgCalls(
-            self.userbot1,
-            cache_duration=100,
-        )
-        self.userbot2 = Client(
-            name="BrandrdXMusic2",
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
-            session_string=str(config.STRING2),
-        )
-        self.two = PyTgCalls(
-            self.userbot2,
-            cache_duration=100,
-        )
-        self.userbot3 = Client(
-            name="BrandrdXMusic3",
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
-            session_string=str(config.STRING3),
-        )
-        self.three = PyTgCalls(
-            self.userbot3,
-            cache_duration=100,
-        )
-        self.userbot4 = Client(
-            name="BrandrdXMusic4",
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
-            session_string=str(config.STRING4),
-        )
-        self.four = PyTgCalls(
-            self.userbot4,
-            cache_duration=100,
-        )
-        self.userbot5 = Client(
-            name="BrandrdXMusic5",
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
-            session_string=str(config.STRING5),
-        )
-        self.five = PyTgCalls(
-            self.userbot5,
-            cache_duration=100,
-        )
+class Call(PyTgCalls): 
+    def __init__(self): 
+        self.userbot1 = Client( 
+            name="BrandrdXMusic1", 
+            api_id=config.API_ID, 
+            api_hash=config.API_HASH, 
+            session_string=str(config.STRING1), 
+        ) 
+        self.one = PyTgCalls( 
+            self.userbot1, 
+            cache_duration=100, 
+        ) 
+        self.userbot2 = Client( 
+            name="BrandrdXMusic2", 
+            api_id=config.API_ID, 
+            api_hash=config.API_HASH, 
+            session_string=str(config.STRING2), 
+        ) 
+        self.two = PyTgCalls( 
+            self.userbot2, 
+            cache_duration=100, 
+        ) 
+        self.userbot3 = Client( 
+            name="BrandrdXMusic3", 
+            api_id=config.API_ID, 
+            api_hash=config.API_HASH, 
+            session_string=str(config.STRING3), 
+        ) 
+        self.three = PyTgCalls( 
+            self.userbot3, 
+            cache_duration=100, 
+        ) 
+        self.userbot4 = Client( 
+            name="BrandrdXMusic4", 
+            api_id=config.API_ID, 
+            api_hash=config.API_HASH, 
+            session_string=str(config.STRING4), 
+        ) 
+        self.four = PyTgCalls( 
+            self.userbot4, 
+            cache_duration=100, 
+        ) 
+        self.userbot5 = Client( 
+            name="BrandrdXMusic5", 
+            api_id=config.API_ID, 
+            api_hash=config.API_HASH, 
+            session_string=str(config.STRING5), 
+        ) 
+        self.five = PyTgCalls( 
+            self.userbot5, 
+            cache_duration=100, 
+        ) 
 
-    async def pause_stream(self, chat_id: int):
-        assistant = await group_assistant(self, chat_id)
-        await assistant.pause_stream(chat_id)
+    async def pause_stream(self, chat_id: int): 
+        assistant = await group_assistant(self, chat_id) 
+        await assistant.pause_stream(chat_id) 
 
-    async def mute_stream(self, chat_id: int):
-        assistant = await group_assistant(self, chat_id)
-        await assistant.mute_stream(chat_id)
+    async def mute_stream(self, chat_id: int): 
+        assistant = await group_assistant(self, chat_id) 
+        await assistant.mute_stream(chat_id) 
 
-    async def unmute_stream(self, chat_id: int):
-        assistant = await group_assistant(self, chat_id)
-        await assistant.unmute_stream(chat_id)
+    async def unmute_stream(self, chat_id: int): 
+        assistant = await group_assistant(self, chat_id) 
+        await assistant.unmute_stream(chat_id) 
 
-    async def get_participant(self, chat_id: int):
-        assistant = await group_assistant(self, chat_id)
-        participant = await assistant.get_participants(chat_id)
-        return participant
+    async def get_participant(self, chat_id: int): 
+        assistant = await group_assistant(self, chat_id) 
+        participant = await assistant.get_participants(chat_id) 
+        return participant 
 
-    async def resume_stream(self, chat_id: int):
-        assistant = await group_assistant(self, chat_id)
-        await assistant.resume_stream(chat_id)
+    async def resume_stream(self, chat_id: int): 
+        assistant = await group_assistant(self, chat_id) 
+        await assistant.resume_stream(chat_id) 
 
-    async def stop_stream(self, chat_id: int):
-        assistant = await group_assistant(self, chat_id)
-        try:
-            await _clear_(chat_id)
-            await assistant.leave_group_call(chat_id)
-        except:
-            pass
+    async def stop_stream(self, chat_id: int): 
+        assistant = await group_assistant(self, chat_id) 
+        try: 
+            await _clear_(chat_id) 
+            await assistant.leave_group_call(chat_id) 
+        except: 
+            pass 
 
-    async def stop_stream_force(self, chat_id: int):
-        try:
-            if config.STRING1:
-                await self.one.leave_group_call(chat_id)
+    async def stop_stream_force(self, chat_id: int): 
+        try: 
+            if config.STRING1: 
+                await self.one.leave_group_call(chat_id) 
             if config.STRING2:
-                await self.two.leave_group_call(chat_id)
-            if config.STRING3:
-                await self.three.leave_group_call(chat_id)
-            if config.STRING4:
-                await self.four.leave_group_call(chat_id)
-            if config.STRING5:
-                await self.five.leave_group_call(chat_id)
-        except Exception as e:
-            print(f"Error: {e}")
